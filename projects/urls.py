2c971178a96f839f5cf39760dbbee753b764070e
Riley Schoppa <rlieyschoppa@yahoo.com>
2020-02-06 16:48:59 -0500

    added image upload saving and displaying image

--- projects/urls.py
+++ projects/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('filtering', views.filtering, name='filtering'),
+    path('filter', views.filter, name='filter'),
 ]
+
--- projects/views.py
+++ projects/views.py
@@ -4,10 +4,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Context, loader
 from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the projects index.")
 
 
+@csrf_exempt
 def filtering(request):
     return render(request, 'filtering.html')
+
+
+@csrf_exempt
+def filter(request):
+    f = request.FILES['image']
+
+    path = settings.MEDIA_ROOT + '/' + f.name
+    with open(path, 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+    #TODO: Do image filtering heere
+
+    return render(request, 'filtering.html', {'prefilter': '/media/' + f.name})
